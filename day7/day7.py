d7ed8c29a7f5e6d3922ab1d1cdd739e578290dad
AlexeyKhoroshenkiy <alexeyk4@nycstudents.net>
2025-12-05 14:01:52 -0500

    day7

--- day7/day7.py
+++ day7/day7.py
@@ -15,10 +15,49 @@ def find_set_vars():
             i += 1
             continue
             
-        value = re.findall(r"^[0-9]+", line)[0]
+        value = int(re.findall(r"^[0-9]+", line)[0])
         var = re.findall("[a-z]+$", line)[0]
         all_vars[var] = value
         data.remove(line)
 
-        
+def find_and():
+    i = 0
+    while i < len(data):
+        line = data[i]
+        existing_vars = "|".join(all_vars.keys())
+        if re.match(r"^([0-9]|" + existing_vars + ")+ AND (" + existing_vars + ")+ -> [a-z]+$", line) is None:
+            i += 1
+            continue
+
+        value1 :str = re.findall(r"^.+ AND", line)[0][0:-4]
+        value2 = re.findall(r"AND [a-z]+", line)[0][4:]
+        var = re.findall("[a-z]+$", line)[0]
+        if value1.isdigit():
+            all_vars[var] = int(value1) & all_vars[value2]
+        else: all_vars[var] = all_vars[value1] & all_vars[value2]
+        data.remove(line)
+
+
+def find_or():
+    i = 0
+    while i < len(data):
+        line = data[i]
+        existing_vars = "|".join(all_vars.keys())
+        if re.match(r"^([0-9]|" + existing_vars + ")+ OR (" + existing_vars + ")+ -> [a-z]+$", line) is None:
+            i += 1
+            continue
+
+        value1 :str = re.findall(r"^.+ OR", line)[0][0:-4]
+        value2 = re.findall(r"OR [a-z]+", line)[0][4:]
+        var = re.findall("[a-z]+$", line)[0]
+        print(value1)
+        if value1.isdigit():
+            all_vars[var] = int(value1) | all_vars[value2]
+        else: all_vars[var] = all_vars[value1] | all_vars[value2]
+        data.remove(line)
+
 find_set_vars()
+find_and()
+find_or()
+
+print(all_vars)
